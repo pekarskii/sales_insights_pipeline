b46bb047f82e16d157592c7fcd43d5af9debf360
andrey <paguyc@gmail.com>
2025-01-21 22:02:55 +0500

    описательная часть скрипта

---
+++ etl_script.py
@@ -0,0 +1,7 @@
+
+Работайте над задачей.
+
+Разработайте скрипт Python для выполнения ETL, создайте необходимые файлы в ветке etl-process.
+
+git add etl_script.py
+git commit -m "Closes #1: добавлен ETL-скрипт"
